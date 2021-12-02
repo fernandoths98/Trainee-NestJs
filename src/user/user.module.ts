399f62ebc636350e9c9ecf1fc8b32070e9e9a565
fernandoths98 <fernandoteguh31@gmail.com>
2021-12-02 19:14:41 +0700

    first commit

---
+++ src/user/user.module.ts
@@ -0,0 +1,12 @@
+import { User } from './models/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+
+@Module({
+    imports:[
+      TypeOrmModule.forFeature([User])
+    ],
+    controllers: [UserController]
+})
+export class UserModule {}
